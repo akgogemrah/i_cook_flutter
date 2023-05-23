3550f410af308519c229eea340d985b22aef5483
akgogemrah <emrahakgog24@gmail.com>
2023-05-23 04:49:10 +0300

    to adding onBoardingPage

---
+++ lib/Styles/OnboardingStyle/OnboardingStyle.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+TextStyle buildOnBoardingBodyTextStyle() {
+  return TextStyle(
+      color: Colors.indigo,
+      fontWeight: FontWeight.bold,
+      fontSize: 25
+  );
+}
+TextStyle buildTextStyleOnboardingSmall() => TextStyle(fontSize: 20,color: Colors.grey);
