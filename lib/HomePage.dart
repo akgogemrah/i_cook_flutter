707d2eb8d0c5c2fb61f45b95346b9f929c7b1e35
akgogemrah <emrahakgog24@gmail.com>
2023-06-02 02:55:34 +0300

    to  create homePage

---
+++ lib/HomePage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'Services/AuthService.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "HomePage",
+            ),
+            ElevatedButton(onPressed: ()async{
+              await Provider.of<Auth>(context,listen: false).signOut();
+
+              setState(() {
+                Provider.of<Auth>(context,listen: false).logout();
+              });
+              print(Provider.of<Auth>(context,listen: false).isLoggedIn);
+
+            }, child:Text("LogOut"))
+          ],
+        ),
+      ),
+
+    );
+  }
+}
